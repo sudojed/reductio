49f9c0ad9038215d6db465b0eb48aeaaa386cabd
abnerLouren <abnerjaredejede@gmail.com>
2025-08-30 17:51:21 +0100

    define the Reductio class constructor and the init version of reduct method

--- src/main/java/com/reductio/Reductio.java
+++ src/main/java/com/reductio/Reductio.java
@@ -1,6 +1,17 @@
 package com.reductio;
 
+import com.reductio.funcoes.Function;
+
+
 public class Reductio {
    
+    private String expr;
     
+    public Reductio(String expr){
+        this.expr = expr;
+    }
+    
+    public Function reduct(){
+        return new Function();
+    }
 }
---
+++ src/main/java/com/reductio/funcoes/Function.java
@@ -0,0 +1,7 @@
+package com.reductio.funcoes;
+
+public class Function{
+    public Function(){
+        
+    }
+}
