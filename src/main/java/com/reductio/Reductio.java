31a15f3bb34fb306fbdd3a75f9ce55ee0e369768
abnerLouren <abnerjaredejede@gmail.com>
2025-09-02 13:51:19 +0100

    define Termo class

--- src/main/java/com/reductio/Reductio.java
+++ src/main/java/com/reductio/Reductio.java
@@ -2,29 +2,44 @@ package com.reductio;
 
 import com.reductio.funcoes.Function;
 
+class Termo {
+    private Double coeficiente;
+    private String variavel;
+    private Double expoente;
 
-public class Reductio {
-   
-    private String expr;
-    
-    public Reductio(String expr){
-        this.expr = expr;
-    }
-    
-    public Reductio reduct(){
-
-        return new Reductio(this.expr);
-        
+    public Termo(Double coeficiente, String variavel, Double expoente) {
+        this.coeficiente = coeficiente;
+        this.variavel = variavel;
+        this.expoente = expoente;
     }
 
-    public Function indendify(){
-
-        return new Function();
-    
-    }
-
-    public void format(){
-        
+    @Override
+    public String toString() {
+        return coeficiente + (variavel == null ? "" : variavel + "^" + expoente);
     }
 }
- 
+
+public class Reductio {
+
+    private String expr;
+
+    public Reductio(String expr) {
+        this.expr = expr;
+    }
+
+    public Reductio reduct() {
+
+        return new Reductio(this.expr);
+
+    }
+
+    public Function indendify() {
+
+        return new Function();
+
+    }
+
+    public void format() {
+
+    }
+}
